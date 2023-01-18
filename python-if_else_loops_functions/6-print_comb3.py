172abc2a7ba835949a334e26221d13b140930ccb
EMME-ESSE <matimancini@gmail.com>
2023-01-18 12:54:06 -0800

    fixed 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
+for x in range(9):
     for n in range(i + 1, 10):
-        if i == 8 and n == 9:
-            print("{:d}{:d}".format(i, n))
+        if x == 8 and n == 9:
+            print("{:d}{:d}".format(x, n))
         else:
-            print("{:d}{:d}".format(i, n), end=', ')
+            print("{:d}{:d}".format(x, n), end=', ')
