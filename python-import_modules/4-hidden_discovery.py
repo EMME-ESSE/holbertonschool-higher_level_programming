b3635256ca6cc2dff30ef36216aba083ddca13db
EMME-ESSE <matimancini@gmail.com>
2023-01-19 08:29:46 -0800

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     import hidden_4
-    length = len(argv)        
-    if length > 1:
-        for i in range(1, length):
+    lengt = len(argv)        
+    if lengt > 1:
+        for i in range(1, lengt):
             if argv[2] != '_':
                 print("{}".format(argv[i]))
