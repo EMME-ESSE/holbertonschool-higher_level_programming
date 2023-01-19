412371c911e632fff0ef4c05503457f9a45636ff
EMME-ESSE <matimancini@gmail.com>
2023-01-19 08:10:21 -0800

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    res = 0
+    if length == 1:
+       	print("{}".format(0))
+    elif length > 1:
+        for i in range(1, length):
+    	    res += int(argv[i])
+        print("{}".format(res))
