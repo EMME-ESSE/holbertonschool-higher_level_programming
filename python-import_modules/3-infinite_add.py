4c3ed5671fb01a1f19e35a53ef026ce08bae1fdc
EMME-ESSE <matimancini@gmail.com>
2023-01-19 17:46:49 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     length = len(argv)
     res = 0
     if length == 1:
-       	print("{}".format(0))
+        print("{}".format(0))
     elif length > 1:
         for i in range(1, length):
-    	    res += int(argv[i])
+            res += int(argv[i])
         print("{}".format(res))
