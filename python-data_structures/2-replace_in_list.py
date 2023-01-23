8169d7a57a87bdfa0de8a95f33f2ec479892a1f3
EMME-ESSE <matimancini@gmail.com>
2023-01-23 06:21:59 -0800

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return(None)
+        return(my_list)
     if idx > len(my_list):
-        return(None)
+        return(my_list)
     else:
         for i in range(len(my_list)):
             if my_list[idx] in [idx]:
