d17e31c9cbec8381661247f20031270571bdbf4f
EMME-ESSE <matimancini@gmail.com>
2023-01-28 11:22:12 -0800

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,6 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print(my_list[i], end='')
             i += 1
-        print()
     except IndexError:
         pass
     print("")
