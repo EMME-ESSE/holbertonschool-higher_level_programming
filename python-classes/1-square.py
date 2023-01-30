650abbc0548049be7c9f91752a5bb812c4c1a47d
EMME-ESSE <matimancini@gmail.com>
2023-01-30 08:41:05 -0800

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""class Square that defines a square by: (based on 0-square.py)"""
+"""Class Square that defines a square by: (based on 0-square.py)"""
 
 
 class Square:
-    """
-    The class define a square object.
-    """
-        def __init__(self, size):
-	    self.__size = size
+    """The class define a square object"""
+    def __init__(self, size):
+        self.__size = size
