f14e8e01fe6865633be1301abbea324b83b3ca58
EMME-ESSE <matimancini@gmail.com>
2023-01-30 08:19:16 -0800

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    The class defines a square object
-    """
     def __init__(self):
-        """
-        Initializes a new instance of the class
-        """
         pass
