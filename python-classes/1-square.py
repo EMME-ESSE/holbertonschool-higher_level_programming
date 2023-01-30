5a93fe7856baf94fbac112e2b22e4caa6bda1131
EMME-ESSE <matimancini@gmail.com>
2023-01-30 08:26:44 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+	"""
+	The class define a square object.
+	"""
+	def __init__(self, size):
+		"""
+		Initializes a new instance of square
+		class with a new size
+		
+		
+		Args: size(int): Size of the square
+		"""
+		self.__size = size
