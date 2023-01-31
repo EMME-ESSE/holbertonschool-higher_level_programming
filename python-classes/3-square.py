6499aad1b036256cfc8f8eaf01105d23b0d5db44
EMME-ESSE <matimancini@gmail.com>
2023-01-31 11:54:09 -0800

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """class Square"""
-    def __init__(self, size=0)
+    def __init__(self, size=0):
         if isinstance(size, int):
             self.__size = size
         else:
-            raise: TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         
