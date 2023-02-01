5db9296de0a406b4b2a846e1b938f5c9c6185490
EMME-ESSE <matimancini@gmail.com>
2023-02-01 10:58:44 -0800

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""class Rectangle"""
+"""class rectangle"""
 
 
-class Rectanlge:
+class Rectangle:
     """The rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
