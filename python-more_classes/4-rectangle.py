6f8a493dc8b7fd5d501166fc02f2cc998600d3a9
EMME-ESSE <spiritexdi@gmail.com>
2023-02-02 15:05:28 -0300

    task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -46,8 +46,8 @@ class Rectangle:
             return a
         else:
             for x in range(self.__height):
-                a += ("#" * self.__width) 
-                if (x < self.__height -1):
+                a += ("#" * self.__width)
+                if (x < self.__height - 1):
                     a += ("\n")
         return a
 
