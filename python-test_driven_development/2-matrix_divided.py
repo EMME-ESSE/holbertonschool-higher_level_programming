efd1e0b00110a79c23a99aa39c800e2b156904cb
EMME-ESSE <spiritexdi@gmail.com>
2023-02-02 18:03:59 -0300

    task 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Divides a given matrix by a given number"""
+
+
+def matrix_divided(matrix, div):
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(matrix) != list or not all(map(lambda x: type(x) == list, matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(map(lambda x: len(x) == len(matrix[0]), matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not all(map(lambda x: all(map(lambda y: type(y) in [int, float], x)), matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    return [[round(y / div, 2) for y in x] for x in matrix]
