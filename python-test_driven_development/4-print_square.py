0c21d3829fd0bdef13581b39523a91f56d0cec59
EMME-ESSE <spiritexdi@gmail.com>
2023-02-04 17:47:15 -0300

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,5 +8,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size):
-        print("#" * size)
+    else:
+        for i in range(size):
+            print("#" * size)
