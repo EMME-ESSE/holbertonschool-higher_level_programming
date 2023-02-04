0aa79183d6d781bea05a7aea02cca846fb26cf7d
EMME-ESSE <matimancini@gmail.com>
2023-02-04 12:55:50 -0800

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Task 4"""
+"""Write a class Square that defines a square"""
 
 
 def print_square(size):
-    """Function that prints a square with the character"""
-    if type(size) != int:
+    "Function that prints a square with the character #"
+    if not size:
+        return
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
