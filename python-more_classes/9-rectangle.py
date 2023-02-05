76c80c9a7ae53da0d81dcfc935b0b6f72073d7ca
EMME-ESSE <spiritexdi@gmail.com>
2023-02-05 01:15:01 -0300

    task 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
     @property
     def height(self):
         """ get height """
-        return self.__self
+        return self.__height
 
     @height.setter
     def height(self, value):
