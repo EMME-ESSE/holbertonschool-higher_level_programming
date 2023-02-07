84124fffaf76d2d1bb26aa623f5d4c098df88073
EMME-ESSE <spiritexdi@gmail.com>
2023-02-07 19:25:47 -0300

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""inherits_from Module"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance
+    of the class that inherited from"""
+    return isinstance(obj, a_class) and not type(obj) is a_class
