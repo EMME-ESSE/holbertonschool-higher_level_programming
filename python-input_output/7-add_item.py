4753cdd8a2ce9a53b02be40348c90d49e65e85ff
EMME-ESSE <spiritexdi@gmail.com>
2023-02-11 11:08:03 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,15 +4,14 @@
 
 from sys import argv
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-adda = "add_item.json"
+
 try:
-    item = load_from_json_file(adda)
+    item = load_from_json_file("add_item.json")
 except FileNotFoundError:
     item = []
 for i in range(1, len(sys.argv)):
-    item += argv[1:]
-save_to_json_file(item, adda)
+    item.append(sys.argv[i])
+save_to_json_file(item, "add_item.json")
