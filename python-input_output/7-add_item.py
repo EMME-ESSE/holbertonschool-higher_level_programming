90ac7d56938898c30a79882ae5029f4229f5fab2
EMME-ESSE <spiritexdi@gmail.com>
2023-02-11 11:02:46 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     item = load_from_json_file("add_item.json")
-except Exception:
+except FileNotFoundError:
     item = []
 for i in range(1, len(sys.argv)):
     item += argv[1:]
