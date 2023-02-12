986616f0e025c201c44b3eea4011cccda34e005d
EMME-ESSE <spiritexdi@gmail.com>
2023-02-12 15:04:22 -0300

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """init function of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """function to_json"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            if all(isinstance(at, str) for at in attrs):
+                if isinstance(attrs, list):
+                    return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
