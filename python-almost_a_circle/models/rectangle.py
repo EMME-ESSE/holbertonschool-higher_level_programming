450bbd5801d3448f8f35c6fb152c9c48a9e97229
EMME-ESSE <spiritexdi@gmail.com>
2023-02-16 20:30:34 -0300

    updated

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,7 +72,6 @@ class Rectangle(Base):
         for i in range(self.__height):
             print((" " * self.__x) + ("#" * self.__width))
 
-
     def __str__(self):
         """STR"""
         id = self.id
