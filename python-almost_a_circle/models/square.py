efe4eef16b1ad21ddbd9af1f7ec3403a98d2eaf7
EMME-ESSE <spiritexdi@gmail.com>
2023-02-17 18:08:58 -0300

    Updated

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -55,8 +55,7 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Dictionary"""
         return {'id': self.id,
-                'width': self.width,
-                'height': self.height,
+                'size': self.size,
                 'x': self.x,
                 'y': self.y
                 }
