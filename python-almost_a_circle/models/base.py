f4ce691fcbccdea28ba42cfc6811f7579ab7c4d5
EMME-ESSE <spiritexdi@gmail.com>
2023-02-17 20:19:16 -0300

    task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,10 @@ class Base:
         json_str = cls.to_json_string(obj_list)
         with open(filename, "w") as f:
             f.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string:
+            return json.loads(json_string)
+        else:
+            return []
