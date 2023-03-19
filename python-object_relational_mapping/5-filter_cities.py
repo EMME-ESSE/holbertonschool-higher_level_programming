dd27298c79db6110c705de81dca905227a1b460d
EMME-ESSE <spiritexdi@gmail.com>
2023-03-19 20:54:30 -0300

    task 5?

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,26 +6,34 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} username password database state"
+        print("Usage: {} username password database state_name"
               .format(sys.argv[0]))
         exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id "
-                "WHERE states.name = %s "
-                "ORDER BY cities.id ASC", (sys.argv[4],))
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id"
+                " = states.id WHERE states.name = %s ORDER BY cities.id ASC",
+                  (state_name,))
 
+    cities = []
     for row in cur.fetchall():
-        print(row)
+        cities.append(row[0])
+
+    print(", ".join(cities))
 
     cur.close()
     conn.close()
+
