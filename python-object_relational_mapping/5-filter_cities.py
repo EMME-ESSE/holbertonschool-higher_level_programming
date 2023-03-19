a65fa4394f76c6ed657edbccbf2c17fed167ceac
EMME-ESSE <spiritexdi@gmail.com>
2023-03-19 20:46:01 -0300

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} username password database state".format(sys.argv[0]))
+        print("Usage: {} username password database state"
+              .format(sys.argv[0]))
         exit(1)
 
     conn = MySQLdb.connect(
