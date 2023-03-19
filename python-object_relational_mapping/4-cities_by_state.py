923418de9110e2e328d807fe4d16952029ac9767
EMME-ESSE <spiritexdi@gmail.com>
2023-03-19 20:19:47 -0300

    fixed

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,21 +9,19 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to database
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user,
                           passwd=mysql_password, db=database_name)
 
-    # Execute SQL query
     cursor = db.cursor()
+    
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities"
                     "JOIN states ON cities.state_id = "
                     "states.id ORDER BY cities.id ASC")
+    
     rows = cursor.fetchall()
 
-    # Display results
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
     cursor.close()
     db.close()
