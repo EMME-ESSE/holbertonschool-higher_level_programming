6007207cbc7e73da9d6015aab9fb254032c452ca
EMME-ESSE <spiritexdi@gmail.com>
2023-03-19 13:24:49 -0300

    fixed

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,13 +1,7 @@
 -- SQL task 15
-SELECT 
-  tv_shows.title
-FROM tv_shows
-WHERE 
-  EXISTS (
-    SELECT 1 
-    FROM tv_show_genres 
-    INNER JOIN tv_genres 
-      ON tv_show_genres.genre_id = tv_genres.id 
-    WHERE tv_show_genres.show_id = tv_shows.id AND tv_genres.name = 'Comedy'
-  )
+SELECT tv_shows.title
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
