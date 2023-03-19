14ac3655a9a9dcb9bde22588d67800ce5ca03397
EMME-ESSE <spiritexdi@gmail.com>
2023-03-19 20:45:44 -0300

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""task 5"""
+"""Task 5"""
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) != 5:
-        print("Usage: {} username password database state_name"
-              .format(args[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state".format(sys.argv[0]))
         exit(1)
 
-    db = MySQLdb.connect(
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=args[1],
-        passwd=args[2],
-        db=args[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    cursor = db.cursor()
-    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN "
-    "states ON cities.state_id"
-    "= states.id WHERE states.name=%s ORDER BY cities.id ASC"
-    cursor.execute(query, (args[4],))
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (sys.argv[4],))
 
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
