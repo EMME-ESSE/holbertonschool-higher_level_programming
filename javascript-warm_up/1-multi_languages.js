0a04ea738a12c43fa04c6a60ef3f9a3269115cd3
EMME-ESSE <spiritexdi@gmail.com>
2023-04-12 18:38:45 -0300

    .

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const v1 = "C is fun";
-const v2 = "Python is cool";
-const v3 = "JavaScript is amazing";
+const v1 = 'C is fun';
+const v2 = 'Python is cool';
+const v3 = 'JavaScript is amazing';
 console.log(`${v1}\n${v2}\n${v3}`);
