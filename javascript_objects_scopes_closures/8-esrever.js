de6ae134ca9a739cd04ac94b33020c195ce81f62
EMME-ESSE <spiritexdi@gmail.com>
2023-04-14 17:53:42 -0300

    .

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-exports.esrever = function(list) {
-    let reversed = [];
-    let i = list.length - 1
-    for (i; i >= 0; i--) {
-      reversed.push(list[i]);
-    }
-    return reversed;
+exports.esrever = function (list) {
+  const reversed = [];
+  let i = list.length - 1;
+  for (i; i >= 0; i--) {
+    reversed.push(list[i]);
   }
+  return reversed;
+};
