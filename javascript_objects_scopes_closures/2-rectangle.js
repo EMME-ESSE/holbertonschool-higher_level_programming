424c001b200c5c12f80b98e51d485811e42c50ed
EMME-ESSE <spiritexdi@gmail.com>
2023-04-14 17:45:36 -0300

    task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) {
-      return {};
+      return ;
     } else {
       this.width = w;
       this.height = h;
