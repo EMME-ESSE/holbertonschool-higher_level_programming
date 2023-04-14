1f811f27c60f2205b8a6f47c3c0e2589510e77c2
EMME-ESSE <spiritexdi@gmail.com>
2023-04-14 10:28:08 -0300

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -7,7 +7,7 @@ const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
 if (isNaN(arg1) || isNaN(arg2)) {
-  console.log("NaN");
+  console.log('NaN');
 } else {
   console.log(add(arg1, arg2));
 }
