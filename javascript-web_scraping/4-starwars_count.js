fa0fbdca6c89fafbdc83c74654291e261071526e
EMME-ESSE <spiritexdi@gmail.com>
2023-04-17 12:33:01 -0300

    .

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,14 +6,13 @@ const characterId = 18;
 request.get(url, (err, res, body) => {
   if (err) {
     console.error(err);
-  }
-  const films = JSON.parse(body).results;
-  let count = 0;
-  for (const film of films) {
-    const characters = film.characters;
-    if (characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
-      count++;
-    }
-  }
-  console.log(count);
+  } 
+    const films = JSON.parse(body);
+    let count = 0;
+      if (films.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
+        count++;
+      }
+    
+    console.log(count);
+
 });
