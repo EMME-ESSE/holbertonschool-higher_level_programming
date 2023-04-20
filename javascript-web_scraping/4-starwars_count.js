f9982754879f7aa033d15e7bd5d7add14bb97d00
EMME-ESSE <spiritexdi@gmail.com>
2023-04-20 20:10:33 -0300

    .

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const [, , url] = process.argv;
 const request = require('request');
-if (args[0] === 'https://swapi-api.hbtn.io/api/films/') {
-  args[0] = 'https://swapi-api.alx-tools.com/api/films/';
-}
-request.get(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const film of films) {
-      const characters = film.characters;
-      if (characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
-        count++;
-      }
-    }
-    console.log(count);
+
+const SWAPI_ORIGINAL_URL = 'https://swapi-api.hbtn.io/api/films/';
+const SWAPI_ALX_URL = 'https://swapi-api.alx-tools.com/api/films/';
+const apiUrl = url === SWAPI_ORIGINAL_URL ? SWAPI_ALX_URL : url;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  for (const film of films) {
+    if (film.characters.some(character => character.endsWith('/18/'))) {
+      count++;
+    }
+  }
+
+  console.log(count);
 });
